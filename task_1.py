8897d7420317846022228e9f906963d245b59803
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-12-09 00:50:31 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,9 +2,9 @@ def is_palindrome(text):
     palindrome = True
     text = text.lower().split()
     new =""
-    new.join(text)
+    for i in range(len(text)):
+        new = new + text[i]
     for i in range(len(new)//2):
         if new[i] != new[-(i+1)]:
             palindrome = False
-
     return palindrome
