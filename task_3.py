d205c5620889717ef463f8a725c346db60c12ce5
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-12-09 00:42:41 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,14 @@
 def min_max(data):
-    return 0, 0
+    if data[0] < data[1]:
+        smol = data[0]
+        big = data[1]
+    else:
+        smol = data[1]
+        big = data[0]
+    for i in range(2,len(data)):
+        elem = data[i]
+        if elem >= big:
+            big = elem
+        elif elem <= smol:
+            smol = elem
+    return smol, big
