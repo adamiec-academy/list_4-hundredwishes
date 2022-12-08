843e550adffbad016d5df32fa6c21ae1db48b11a
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-12-09 00:42:04 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,14 @@
 def my_split(text):
-    return [text]
+    list = []
+    text = text.strip()
+    word = ""
+    for i in range(len(text)):
+        if text[i] == " ":
+            list.append(word)
+            word = ""
+        elif (i+1) == len(text):
+            word = word + text[i]
+            list.append(word)
+        else:
+            word = word + text[i]
+    return list
