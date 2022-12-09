9fb4c67245a851af88e02c1dd3fc909f879a7b5d
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-12-09 01:03:05 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,14 +1,18 @@
 def min_max(data):
-    if data[0] < data[1]:
-        smol = data[0]
-        big = data[1]
+    if len(data)>1:
+        if data[0] < data[1]:
+            smol = data[0]
+            big = data[1]
+        else:
+            smol = data[1]
+            big = data[0]
+        for i in range(2,len(data)):
+            elem = data[i]
+            if elem >= big:
+                big = elem
+            elif elem <= smol:
+                smol = elem
     else:
-        smol = data[1]
+        smol = data[0]
         big = data[0]
-    for i in range(2,len(data)):
-        elem = data[i]
-        if elem >= big:
-            big = elem
-        elif elem <= smol:
-            smol = elem
     return smol, big
